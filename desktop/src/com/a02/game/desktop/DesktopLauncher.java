f5d96be793854168f3820a114c026bc3e6c49b72
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2020-10-17 17:17:45 +0200

    Dibujado inicial y movimiento con ratón

--- desktop/src/com/a02/game/desktop/DesktopLauncher.java
+++ desktop/src/com/a02/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.a02.game.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 640;
+		config.height = 360;
+		config.fullscreen = false;
+
 		new LwjglApplication(new MainGame(), config);
 	}
 }
