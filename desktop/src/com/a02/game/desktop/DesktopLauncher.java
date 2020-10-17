f88ce43736065d76c108bfe28b9cc67018f91e30
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2020-10-17 16:13:09 +0200

    First commit

---
+++ desktop/src/com/a02/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.a02.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.a02.game.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
