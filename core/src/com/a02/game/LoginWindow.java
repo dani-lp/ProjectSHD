33ded9381827fb3a09f05bf6614374d8d13739a7
Rubén García Pedrejón <rubengp@opendeusto.es>
2020-10-23 12:44:54 +0200

    Ventana login v0.0

---
+++ core/src/com/a02/game/LoginWindow.java
@@ -0,0 +1,44 @@
+package com.a02.game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Login");
+        frame.setSize(640,360);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3,2));
+
+        //Usuario
+        JLabel userLabel = new JLabel("User");
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        panel.add(userText);
+
+        //PW
+        JLabel passwordLabel = new JLabel("Password");
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        panel.add(passwordText);
+
+        //Buttons
+
+        JButton loginButton = new JButton("Login");
+        panel.add(loginButton);
+
+        JButton registerButton = new JButton("Register");
+        panel.add(registerButton);
+
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+
+    }
+}
