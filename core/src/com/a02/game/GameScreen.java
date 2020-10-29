b126418748b3dcb1263c22a8b6087940803ee4ec
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2020-10-29 20:48:23 +0100

    Adaptación de velocidad de enemigos a DeltaTime

--- core/src/com/a02/game/GameScreen.java
+++ core/src/com/a02/game/GameScreen.java
@@ -115,6 +115,7 @@ public class GameScreen implements Screen {
 
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit();
+            System.exit(0);
         }
     }
 
