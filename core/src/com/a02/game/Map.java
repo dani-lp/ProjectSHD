326b073937bdd4bb8bfecac7718511cec63c8f7c
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2020-10-29 18:57:45 +0100

    Mapa inicial

--- core/src/com/a02/game/Map.java
+++ core/src/com/a02/game/Map.java
@@ -2,10 +2,36 @@ package com.a02.game;
 
 public class Map {
     private boolean[][] occGrid; //Cuadricula de ocupación (true si la casilla está ocupada)
-    private Entity[][] entityGrid; //Cuadricula de ocupación (true si la casilla está ocupada)
+    private GameObject[][] entityGrid; //Cuadricula de ocupación (true si la casilla está ocupada)
+    private String sprite;
 
-    protected Map() {
+    protected Map(String sprite) {
         occGrid = new boolean[10][16];
+        entityGrid = new GameObject[10][16];
+        this.sprite = sprite;
     }
 
+    public boolean[][] getOccGrid() {
+        return occGrid;
+    }
+
+    public void setOccGrid(boolean[][] occGrid) {
+        this.occGrid = occGrid;
+    }
+
+    public GameObject[][] getEntityGrid() {
+        return entityGrid;
+    }
+
+    public void setEntityGrid(GameObject[][] entityGrid) {
+        this.entityGrid = entityGrid;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(String sprite) {
+        this.sprite = sprite;
+    }
 }
