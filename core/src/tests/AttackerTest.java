67b9d88b29cd40fa1ef4ffe623279b8a41cf03ce
Rubén García Pedrejón <rubengp@opendeusto.es>
2020-11-02 20:53:42 +0100

    Recuperacion de tests y añadiendo nuevos constructores

---
+++ core/src/tests/AttackerTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import com.a02.game.Attacker;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AttackerTest {
+    Attacker attackerTest = new Attacker(1,2,3,4,"test1.png", 5,"test","electrico",6,true,7,true,false,"Electrico",1.1f);
+    @Test
+    public void getAttackDamage() {
+        assertEquals(1.1f,attackerTest.getAttackDamage());
+    }
+
+
+    @Test
+    public void getAttackType() {
+    }
+
+
+    @Test
+    public void buy() {
+    }
+}
