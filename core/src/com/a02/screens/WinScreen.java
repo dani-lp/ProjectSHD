eded8e69a9b5f07876f0b032a173816628845932
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2021-01-16 19:49:20 +0100

    Fin de pantalla de victoria

--- core/src/com/a02/screens/WinScreen.java
+++ core/src/com/a02/screens/WinScreen.java
@@ -19,20 +19,18 @@ public class WinScreen implements Screen {
     private final MainGame game;
 
     private final int currentPoints; //Pasados entre GameScreen(s)
-    private float animationTimer; //Usado para animaciones
     private final UIButton menuButton; //Para salir al menu
     private final UIButton quitButton; //Para cerrar el juego
     private final UIButton restartButton; //Volver a empezar las rondas normales desde la ronda 1
     private final SoundPlayer soundPlayer; //Para reproducir sonidos de victoria
     private final BitmapFont font; //Fuente para las letras
     private final Animation<TextureRegion> pizzAnimation; //Animacion de celebrando
-    private float count1;//Contadores para las animaciones para que vayan no sincronizadas
-    private float count2;
-    private float count3;
-    private final Texture wallpaperTexture;//Textura del fondo
+    private float count1, count2, count3;//Contadores para las animaciones para que vayan no sincronizadas
+    private double heartXCounter;
+    private final Texture wallpaperTexture, heartTexture, frameTexture;//Texturas del fondo, corazones y marco de puntos
     private final Pixmap pm3;
 
-    public WinScreen(final String username,MainGame game,  final int points) { //game y points se pasan desde GameScreen
+    public WinScreen(final String username, MainGame game, final int points) { //game y points se pasan desde GameScreen
         //Guarda los datos desde un hilo
         Thread t = new Thread(new Runnable() {
             @Override
@@ -45,7 +43,6 @@ public class WinScreen implements Screen {
         this.game = game;
         this.currentPoints = points;
 
-        this.animationTimer = 0;
         //Asignacion de valores por defecto
         restartButton = new UIButton(123, 87, 74, 36,
                 "Buttons/replayButtonIdle.png","Buttons/replayButtonPressed.png");
@@ -60,8 +57,11 @@ public class WinScreen implements Screen {
         count1 = 0;
         count2 = 0.2f;
         count3 = 0.5f;
+        heartXCounter = 0;
 
         wallpaperTexture = new Texture("wallpaperTestOld.png");
+        heartTexture = new Texture("pizzaHeart.png");
+        frameTexture = new Texture("winFrame.png");
 
         pm3 = new Pixmap(Gdx.files.internal("defaultCursor.png"));
     }
@@ -73,6 +73,7 @@ public class WinScreen implements Screen {
         count1 += delta; //Actualizar los contadores
         count2 += delta;
         count3 += delta;
+        heartXCounter += 0.2f;
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -81,16 +82,20 @@ public class WinScreen implements Screen {
 
         game.entityBatch.begin();//Dibujado de los botones y el fondo
         game.entityBatch.draw(wallpaperTexture, 0, 0);
+        game.entityBatch.draw(frameTexture, 90, 128);
         game.entityBatch.draw(menuButton.getCurrentTexture(), menuButton.getX(), menuButton.getY());
         game.entityBatch.draw(quitButton.getCurrentTexture(), quitButton.getX(), quitButton.getY());
         game.entityBatch.draw(restartButton.getCurrentTexture(), restartButton.getX(), restartButton.getY());
 
-        font.draw(game.entityBatch, "Enhorabuenaaa", 100, 160); //Dibujado de los dialogos
-        font.draw(game.entityBatch, "Gracias por salvarnos!!!", 90, 140);
+        font.draw(game.entityBatch, "¡VICTORIA!", 136, 152); //Dibujado de los dialogos
+        font.draw(game.entityBatch, "Puntuación: " + currentPoints, 117, 138);
 
         game.entityBatch.draw(pizzAnimation.getKeyFrame(count1,true),35, 42);   //Dibujado de las animaciones de las pizzas
         game.entityBatch.draw(pizzAnimation.getKeyFrame(count2,true),254, 101);
         game.entityBatch.draw(pizzAnimation.getKeyFrame(count3,true),226, 16);
+        game.entityBatch.draw(heartTexture, 44, (float) Math.sin(heartXCounter) + 77);
+        game.entityBatch.draw(heartTexture, 263, (float) Math.sin(heartXCounter) + 136);
+        game.entityBatch.draw(heartTexture, 235, (float) Math.sin(heartXCounter) + 51);
 
         game.entityBatch.end();
     }
@@ -117,6 +122,9 @@ public class WinScreen implements Screen {
 
     @Override
     public void dispose() {     //Limpiado de lo que hay en la screen
+        wallpaperTexture.dispose();
+        heartTexture.dispose();
+        frameTexture.dispose();
         menuButton.disposeButton();
         quitButton.disposeButton();
         restartButton.disposeButton();
